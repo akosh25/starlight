2bf0f14f2d3d575fc8afd5269b88e4517674465c
Urban Akos <akosh.urban@gmail.com>
2024-04-05 11:56:42 +0200

    php test

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <title>Az első PHP programom</title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="style.css"/>
+  </head>
+  <body>
+    <?php
+		require_once "fejlec.html";
+		require_once "fejlec.html";
+		
+		echo "Hello World! <br/> valami szöveg";
+		
+		require_once "lablec.html";
+	?>
+  </body>
+</html>
