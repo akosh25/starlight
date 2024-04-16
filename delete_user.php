c1129f1a583f3a8443f51d0ffad4f1316d316f8c
Urban Akos <akosh.urban@gmail.com>
2024-04-16 16:27:38 +0200

    user_delete

--- delete_user.php
+++ delete_user.php
@@ -3,16 +3,23 @@
 	include("functions.php");
 	
 	if(isset($_SESSION["user"]) && !empty($_SESSION["user"])){
-		$user_name = $_SESSION["user"]["username"];
-		$query = "DELETE FROM users WHERE username = '$user_name'";
-		if(mysqli_query($conn, $query)){
-			session_unset();
-			session_destroy();
-			
-			header('location: index.php');
-			die;
+		$user_id = $_SESSION["user"]["id"];
+
+		// Töröljük az összes olyan fotót, amelyek ehhez a felhasználóhoz tartoznak
+		$delete_photos_query = "DELETE FROM photos WHERE user_id = $user_id";
+		if(mysqli_query($conn, $delete_photos_query)){
+			// Majd töröljük a felhasználó rekordját
+			$delete_user_query = "DELETE FROM users WHERE id = $user_id";
+			if(mysqli_query($conn, $delete_user_query)){
+				session_unset();
+				session_destroy();
+				header('location: index.php');
+				die;
+			} else {
+				echo "Hiba történt a felhasználói fiók törlése során: " . mysqli_error($conn);
+			}
 		} else {
-			echo "Hiba történt a felhasználói fiók törlése során: " . mysqli_error($conn);
+			echo "Hiba történt a fotók törlése során: " . mysqli_error($conn);
 		}
 	}
 	else{
